584e3dcae44a0e09c8f32099df430858d780fe4e
BekDzyudo <jalilovshohbek9909@gmail.com>
2024-12-26 17:23:13 +0500

    fix

--- src/pages/adabiyotlar/components/main/bookBatafsil/bookBatafsil.jsx
+++ src/pages/adabiyotlar/components/main/bookBatafsil/bookBatafsil.jsx
@@ -4,20 +4,24 @@ import { Link, useParams } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import PageLoader from "../../../../../Loader/PageLoader";
 import useGetFetch from "../../../../../hooks/useGetFetch";
-import { AiFillLike } from "react-icons/ai";
-import { AiFillDislike } from "react-icons/ai";
 import { BiDislike } from "react-icons/bi";
 import { BiLike } from "react-icons/bi";
 import { AuthContext } from "../../../../../contexts/AuthContext";
 import { toast } from "react-toastify";
+import { ToggleButton } from "@mui/material";
 
 export function BookBatafsil() {
-  const {userName} = useContext(AuthContext)
+  const { userName } = useContext(AuthContext);
   const { id } = useParams();
-  const userCommentText = useRef()
-  const replyCommentText = useRef()
-  const regCommentForm = useRef()
-  const replyCommentForm = useRef()
+  // ref
+  const userCommentText = useRef();
+  const regCommentForm = useRef();
+  const replyCommentForm = useRef();
+  // state
+  const [replyText, setReplyText] = useState("");
+  const [like, setLike] = useState(0);
+  const [disLike, setDislike] = useState(0);
+
   const monthArr = [
     "yanvar",
     "fevral",
@@ -32,52 +36,133 @@ export function BookBatafsil() {
     "noyabr",
     "dekabr",
   ];
-  
-// reply comments
+
+  // reply comments
   const [replayComment, setReplayComment] = useState(-1);
   function handleActive(index) {
     setReplayComment(index);
   }
-// adabiyot
+    // reply comments
+    const [replayCommentDetails, setReplayCommentDetails] = useState(-1);
+    function handleActiveDetails(index) {
+      setReplayCommentDetails(index);
+    }
+    // reply comments List
+    const [replayCommentList, setReplayCommentList] = useState(-1);
+    function handleActiveList(index) {
+      setReplayCommentList(index);
+    }
+  // adabiyot
   const { data, isPending, error } = useGetFetch(
     `${import.meta.env.VITE_BASE_URL}/edu-prof/adabiyotlar/${id}/`
   );
+  console.log(data);
 
-function addComment(commentId){
-  let obj ={
-    user: userName.userId,
-    adabiyot: id,
-    text: userCommentText.current.value,
-    reply_comment: commentId,
+  // add comment funck
+  function addComment(commentId) {
+    let obj = {
+      user: userName.userId,
+      adabiyot: id,
+      text: userCommentText.current.value,
+      reply_comment: commentId,
+      is_reply: true
+    };
+    if (!obj.text.trim()) {
+      toast.error("Iltimos bo'sh maydonni to'ldiring");
+    } else {
+      fetch(`${import.meta.env.VITE_BASE_URL}/edu-prof/create-comment/`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(obj),
+      })
+        .then(async (res) => {
+          console.log(res);
+
+          const errorObj = await res.json();
+          if (!res.ok) throw new Error(JSON.stringify(errorObj));
+          return res;
+        })
+        .then((data) => {
+          regCommentForm.current.reset();
+        })
+        .catch((err) => {
+          const errorObj = JSON.parse(err.message);
+          toast.error(errorObj);
+        })
+        .finally(() => {
+          // location.reload()
+        });
+    }
   }
-  if(!obj.text.trim()){
-    toast.error("Iltimos bo'sh maydonni to'ldiring")
-  }
-  else{
-    fetch(`${import.meta.env.VITE_BASE_URL}/edu-prof/create-comment/`, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(obj),
-              })
-                .then(async(res) => {
-                  console.log(res);
-                  
-                  const errorObj = await res.json()
-                  if (!res.ok) throw new Error(JSON.stringify(errorObj));
-                  return res;
-                })
-                .then((data) => {
-                  regCommentForm.current.reset();
-                })
-                .catch((err) => {
-                  const errorObj = JSON.parse(err.message)
-                  toast.error( errorObj)
-                })
-                .finally(() => {
-                  location.reload()
-                });
-  }
-}
+
+  // reply funck
+  const handleReply = (commentId) => {
+    let obj = {
+      user: userName.userId,
+      adabiyot: id,
+      text: replyText,
+      reply_comment: commentId,
+    };
+    console.log(obj);
+
+    if (!replyText.trim()) {
+      toast.error("Iltimos bo'sh maydonni to'ldiring");
+    } else {
+      fetch(`${import.meta.env.VITE_BASE_URL}/edu-prof/create-comment/`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(obj),
+      })
+        .then(async (res) => {
+          const errorObj = await res.json();
+          if (!res.ok) throw new Error(JSON.stringify(errorObj));
+          return res;
+        })
+        .then((data) => {
+          setReplyText("");
+        })
+        .catch((err) => {
+          const errorObj = JSON.parse(err.message);
+          toast.error(errorObj);
+        })
+        .finally(() => {
+          replyCommentForm.current.reset();
+        });
+    }
+  };
+
+  // like and disLike
+  const handleLike = (commentId) => {
+    let obj = {
+      like: like,
+      dislike: disLike,
+      text: replyText,
+    };
+    console.log(obj);
+
+    useEffect(()=>{
+      fetch(`${import.meta.env.VITE_BASE_URL}/edu-prof/update-comment/${commentId}/`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(obj),
+      })
+        .then(async (res) => {
+          const errorObj = await res.json();
+          if (!res.ok) throw new Error(JSON.stringify(errorObj));
+          return res;
+        })
+        .then((data) => {
+          setReplyText("");
+        })
+        .catch((err) => {
+          const errorObj = JSON.parse(err.message);
+          toast.error(errorObj);
+        })
+        .finally(() => {
+          replyCommentForm.current.reset();
+        });
+    }, [])
+  };
 
   return (
     <>
@@ -110,20 +195,38 @@ function addComment(commentId){
               </div>
               <div className="bookDesc">
                 <p className="title">Badiiy adabiyot haqida</p>
-                <p className="titleName">{data.results.adabiyotlar.description}</p>
+                <p className="titleName">
+                  {data.results.adabiyotlar.description}
+                </p>
                 <div className="yuklash">
                   <div className="yuklashDesc">
                     <p>Badiiy adabiyot</p>
                     <p>
                       So’ngi o’zgartirilgan sana:{" "}
                       <span>
-                        {new Date(data.results.adabiyotlar.created_at).getDate()}-
-                        {monthArr[new Date(data.results.adabiyotlar.created_at).getMonth()]}{" "}
-                        {new Date(data.results.adabiyotlar.created_at).getFullYear()}-yil
+                        {new Date(
+                          data.results.adabiyotlar.created_at
+                        ).getDate()}
+                        -
+                        {
+                          monthArr[
+                            new Date(
+                              data.results.adabiyotlar.created_at
+                            ).getMonth()
+                          ]
+                        }{" "}
+                        {new Date(
+                          data.results.adabiyotlar.created_at
+                        ).getFullYear()}
+                        -yil
                       </span>
                     </p>
                   </div>
-                  <Link to={data.results.adabiyotlar.file} target="_blanck" className="yuklashBtn">
+                  <Link
+                    to={data.results.adabiyotlar.file}
+                    target="_blanck"
+                    className="yuklashBtn"
+                  >
                     Yuklab olish
                   </Link>
                 </div>
@@ -132,7 +235,7 @@ function addComment(commentId){
                   <p className="izohTitle">Izohlar</p>
                   <form action="" className="izohForm" ref={regCommentForm}>
                     <textarea
-                    ref={userCommentText}
+                      ref={userCommentText}
                       name=""
                       id="matn"
                       cols="30"
@@ -141,7 +244,13 @@ function addComment(commentId){
                     ></textarea>
                   </form>
                   <div className="saveBlok">
-                    <button className="saveBtn" id="saveMalumot" onClick={()=>addComment(null)}>Yuborish</button>
+                    <button
+                      className="saveBtn"
+                      id="saveMalumot"
+                      onClick={() => addComment(null)}
+                    >
+                      Yuborish
+                    </button>
                   </div>
                 </div>
                 <div className="comments">
@@ -183,19 +292,143 @@ function addComment(commentId){
                                 Javob yozish
                               </button>
                             </div>
-                            {/*  */}
-                            <div className={replayComment == index ? "replyForm" : "replyNone"}>
+                            <div
+                              className={
+                                replayComment == index
+                                  ? "replyForm"
+                                  : "replyNone"
+                              }
+                            >
                               <div className="replyCommentImage">
                                 <img src={comment.user.image} alt="" />
                               </div>
                               <form ref={replyCommentForm}>
-                                <input ref={replyCommentText} type="text" placeholder="kommentariya..."/>
+                                <input
+                                  onChange={(e) => setReplyText(e.target.value)}
+                                  type="text"
+                                  placeholder="Javob yozing..."
+                                />
                                 <div className="replyBtns">
-                                  <button type="button" onClick={()=>setReplayComment(-1)}>Bekor qilish</button>
-                                  <button type="submit" onClick={()=>addComment(comment.id)}>Yuborish</button>
+                                  <button
+                                    type="button"
+                                    onClick={() => setReplayComment(-1)}
+                                  >
+                                    Bekor qilish
+                                  </button>
+                                  <button
+                                    type="button"
+                                    onClick={() => handleReply(comment.id)}
+                                  >
+                                    Yuborish
+                                  </button>
                                 </div>
                               </form>
                             </div>
+                            {/* ===== */}
+                           
+                            {comment.sub_replies.length > 0 &&  <span className="javobSelect"   onClick={()=>handleActiveList((prev)=>prev==index ? -1 : index)}>{comment.sub_replies.length} ta javob</span>}
+                            {replayCommentList == index && (
+                              <div className="comments">
+                              {comment.sub_replies.length > 0 &&
+                                comment.sub_replies.map((commentReplay, index) => {
+                                  return (
+                                    <div key={commentReplay.id} className="comment">
+                                      <div className="commentImage">
+                                        <img src={commentReplay.user.image} alt="" />
+                                      </div>
+                                      <div className="commentDesc">
+                                        <div className="nameAndDate">
+                                          <span>
+                                            {commentReplay.user.first_name +
+                                              " " +
+                                              commentReplay.user.last_name}
+                                          </span>
+                                          <span>
+                                            {new Date(
+                                              commentReplay.created_at
+                                            ).getDate()}
+                                            .
+                                            {new Date(
+                                              commentReplay.created_at
+                                            ).getMonth() + 1}
+                                            .
+                                            {new Date(
+                                              commentReplay.created_at
+                                            ).getFullYear()}
+                                          </span>
+                                        </div>
+                                        <div className="commentTitle">
+                                          {commentReplay.text}
+                                        </div>
+                                        <div className="btns">
+                                          <div className="like">
+                                            <span>
+                                              <BiLike />
+                                            </span>
+                                            <span>{commentReplay.like}</span>
+                                          </div>
+                                          <div className="disLike">
+                                            <span>
+                                              <BiDislike />
+                                            </span>
+                                            <span>{commentReplay.dislike}</span>
+                                          </div>
+                                          <button
+                                            onClick={() => handleActiveDetails(index)}
+                                          >
+                                            Javob yozish
+                                          </button>
+                                        </div>
+                                        {/*  */}
+                                        <div
+                                          className={
+                                            replayCommentDetails == index
+                                              ? "replyForm"
+                                              : "replyNone"
+                                          }
+                                        >
+                                          <div className="replyCommentImage">
+                                            <img
+                                              src={comment.user.image}
+                                              alt=""
+                                            />
+                                          </div>
+                                          <form ref={replyCommentForm}>
+                                            <input
+                                              onChange={(e) =>
+                                                setReplyText(e.target.value)
+                                              }
+                                              type="text"
+                                              placeholder="Javob yozing..."
+                                            />
+                                            <div className="replyBtns">
+                                              <button
+                                                type="button"
+                                                onClick={() =>
+                                                  setReplayCommentDetails(-1)
+                                                }
+                                              >
+                                                Bekor qilish
+                                              </button>
+                                              <button
+                                                type="button"
+                                                onClick={() =>
+                                                  handleReply(comment.id)
+                                                }
+                                              >
+                                                Yuborish
+                                              </button>
+                                            </div>
+                                          </form>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  );
+                                })}
+                            </div>
+                            )}
+                            
+                            {/* ===== */}
                           </div>
                         </div>
                       );
